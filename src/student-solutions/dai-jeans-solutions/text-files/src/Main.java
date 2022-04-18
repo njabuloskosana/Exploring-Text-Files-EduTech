c9eb279e7ee909921529ae1c83d2c9dddb1e6204
mzujean <101647826+mzujean@users.noreply.github.com>
2022-04-18 22:03:36 +0200

    Append Text
    Using a buffer reader to write the text in.

--- src/student-solutions/dai-jeans-solutions/text-files/src/Main.java
+++ src/student-solutions/dai-jeans-solutions/text-files/src/Main.java
@@ -3,7 +3,9 @@ import java.io.*;
 import java.nio.file.*;;
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextFileOperations operations=new TextFileOperations();
+        TextFileOperations operations = new TextFileOperations();
         System.out.println("Add code to test operations");
+
+        
     }
 }
